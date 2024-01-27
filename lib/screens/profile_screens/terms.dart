a0afa4364a213ccb0c51cf5bf8f21405f539a238
ahmedazim7804 <ahmedazim7804@gmail.com>
2024-01-27 16:45:59 +0530

    fixing some colors

--- lib/screens/profile_screens/terms.dart
+++ lib/screens/profile_screens/terms.dart
@@ -54,7 +54,7 @@ class Terms extends StatelessWidget {
           child: Text(
             'Terms & Conditions',
             style: TextStyle(
-              color: appColors.whiteText,
+              color: Colors.white,
               fontSize: 20,
               fontFamily: 'Epilogue',
               fontWeight: FontWeight.w700,
@@ -84,75 +84,78 @@ class Terms extends StatelessWidget {
         ),
         Align(
           child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 80,
-                ),
-                Text(
-                  'Devcomms Privacy Policy',
-                  style: TextStyle(
-                      fontSize: 18,
-                      color: appColors.text2,
-                      fontWeight: FontWeight.w900,
-                      fontFamily: 'Epilogue'),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'License to Use: By downloading or using the Amulx app, you are granted a limited, non-exclusive, non-transferable license to use the app for your personal, non-commercial use. The app and all associated content remain the property of the apps developers and/or licensors.',
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 80,
+                  ),
+                  Text(
+                    'Devcomms Privacy Policy',
                     style: TextStyle(
-                        fontSize: 14,
+                        fontSize: 18,
                         color: appColors.text2,
-                        fontWeight: FontWeight.bold,
+                        fontWeight: FontWeight.w900,
                         fontFamily: 'Epilogue'),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'In no event will the apps developers and/or licensors be liable for any direct, '
-                    'indirect, incidental, special, consequential, or exemplary damages, '
-                    'including but not limited to damages for loss of profits, goodwill, use, data, or other '
-                    'intangible losses, arising out of or in connection with the Amulx app or any associated content.',
-                    style: TextStyle(
-                        fontSize: 14,
-                        color: appColors.text2,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Epilogue'),
+                  SizedBox(
+                    height: 30,
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'The Amulx app may collect certain personal information from you,'
-                    ' such as your name, email address, and usage data.'
-                    ' The apps developers and/or licensors will use this information in accordance with their privacy policy,'
-                    ' which can be found on the apps website.',
-                    style: TextStyle(
-                        fontSize: 14,
-                        color: appColors.text2,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Epilogue'),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'License to Use: By downloading or using the Amulx app, you are granted a limited, non-exclusive, non-transferable license to use the app for your personal, non-commercial use. The app and all associated content remain the property of the apps developers and/or licensors.',
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: appColors.text2,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Epilogue'),
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'The apps developers and/or licensors reserve the right to modify these terms and '
-                    'conditions at any time, without notice. Your continued use of the Amulx app following any such '
-                    'modifications will constitute your acceptance of the modified terms and conditions.',
-                    style: TextStyle(
-                        fontSize: 14,
-                        color: appColors.text2,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'Epilogue'),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'In no event will the apps developers and/or licensors be liable for any direct, '
+                      'indirect, incidental, special, consequential, or exemplary damages, '
+                      'including but not limited to damages for loss of profits, goodwill, use, data, or other '
+                      'intangible losses, arising out of or in connection with the Amulx app or any associated content.',
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: appColors.text2,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Epilogue'),
+                    ),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'The Amulx app may collect certain personal information from you,'
+                      ' such as your name, email address, and usage data.'
+                      ' The apps developers and/or licensors will use this information in accordance with their privacy policy,'
+                      ' which can be found on the apps website.',
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: appColors.text2,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Epilogue'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'The apps developers and/or licensors reserve the right to modify these terms and '
+                      'conditions at any time, without notice. Your continued use of the Amulx app following any such '
+                      'modifications will constitute your acceptance of the modified terms and conditions.',
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: appColors.text2,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Epilogue'),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         )
