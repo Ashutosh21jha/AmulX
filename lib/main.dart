de9c918dc6fdec96cff871142e8e766f463c71da
Mohit Maulekhi <mohitmaulekhi312@gmail.com>
2024-07-12 14:21:17 +0530

    Added default profile picture and changed default theme to light mode

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ void main() async {
     debugShowCheckedModeBanner: false,
     theme: lightThemeData(),
     darkTheme: darkThemeData(),
-    themeMode: ThemeMode.dark,
+    themeMode: ThemeMode.light,
     home: const SplashScreen(),
   ));
 }
